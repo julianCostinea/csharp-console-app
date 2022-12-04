f450727ddea14b035922c1764ce9e04f0ff64ffc
HOVEDKONTOR\ejc <ejc@arp-hansen.dk>
2022-12-04 17:19:54 +0100

    initial commit, stopwatch exercise

---
+++ ConsoleAppClassPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Tasks;
+using ConsoleAppClassPractice;
+
+namespace ConsoleAppClassPractice
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var stopWatch = new StopWatchExercise();
+            try
+            {
+                stopWatch.StartStopWatch();
+                // stopWatch.StartStopWatch();
+                await Task.Delay(3000);
+                stopWatch.StopStopWatch();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ ConsoleAppClassPractice/StopWatchExercise.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleAppClassPractice;
+
+public class StopWatchExercise
+{
+    private DateTime _timeStamp;
+    public void StartStopWatch()
+    {
+        if (_timeStamp != new DateTime())
+        {
+            throw new ArgumentException("Cannot start stopWatch twice. Must stop first");
+        }
+        _timeStamp = DateTime.Now;
+    }
+
+    public void StopStopWatch()
+    {
+        var timePassed = DateTime.Now - _timeStamp;
+        Console.WriteLine(timePassed);
+        _timeStamp = new DateTime();
+    }
+}
